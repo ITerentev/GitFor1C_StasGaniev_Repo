ba7ee68ef1fba5553b10d31d116b8c35baf078c7
Ivan <Terentev1C@yandex.ru>
2024-12-30 20:49:12 +0300

    батники по уроку

---
+++ Export.bat
@@ -0,0 +1,2 @@
+chcp 65001
+"C:\Program Files (x86)\1cv8t\common\1cestartt.exe" config /F "C:\Users\Awake\Documents\InfoBase3" /NАдминистратор /DumpConfigToFiles "C:\GitReps\Repo1\cfg" -Format Hierarchical
---
+++ Load.bat
@@ -0,0 +1,2 @@
+chcp 65001
+"C:\Program Files (x86)\1cv8t\common\1cestartt.exe" config /F "C:\Users\Awake\Documents\InfoBase3" /NАдминистратор /LoadConfigFromFiles "C:\GitReps\Repo1\cfg" -Format Hierarchical /UpdateDBCfg
